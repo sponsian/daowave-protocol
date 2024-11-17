431c3fe704977afa8c9179e562a671ea2d400135
hichri-louay <louay.hichri@hotmail.com>
2024-11-17 18:36:27 +0100

    rename files and vars

--- contracts/ORCAProtocol/token/ORCAToken.sol
+++ contracts/ORCAProtocol/token/ORCAToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./TokenAccessControl.sol";
 
-contract COToken is ERC20("coordinape.com", "CO"), TokenAccessControl {
+contract ORCAToken is ERC20("orcanize.app", "ORCA"), TokenAccessControl {
 	uint256 immutable private _cap = 1_000_000_000 ether;
 
 	bytes32 private immutable _PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
@@ -20,7 +20,7 @@ contract COToken is ERC20("coordinape.com", "CO"), TokenAccessControl {
     }
 
 	function _mint(address account, uint256 amount) internal virtual override {
-        require(totalSupply() + amount <= cap(), "COToken: cap exceeded");
+        require(totalSupply() + amount <= cap(), "ORCAToken: cap exceeded");
         super._mint(account, amount);
     }
 
@@ -33,7 +33,7 @@ contract COToken is ERC20("coordinape.com", "CO"), TokenAccessControl {
         return keccak256(
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes("coordinape.com")),
+                keccak256(bytes("orcanize.app")),
                 keccak256(bytes('1')),
                 chainId,
                 address(this)
@@ -42,8 +42,8 @@ contract COToken is ERC20("coordinape.com", "CO"), TokenAccessControl {
     }
 
 	function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public {
-        require(block.timestamp <= deadline, "COToken: expired deadline");
-        require(owner != address(0), "COToken: owner can't be ZERO address ");
+        require(block.timestamp <= deadline, "ORCAToken: expired deadline");
+        require(owner != address(0), "ORCAToken: owner can't be ZERO address ");
 
         bytes32 digest = keccak256(
             abi.encode(
@@ -54,18 +54,18 @@ contract COToken is ERC20("coordinape.com", "CO"), TokenAccessControl {
         );
 
         address signer = ECDSA.recover(digest, v, r, s);
-        require(signer == owner, "COToken: invalid signature");
+        require(signer == owner, "ORCAToken: invalid signature");
 
         _approve(owner, spender, value);
     }
 
     function transfer(address _to, uint256 _amount) public override notPaused returns(bool) {
-        require(_to != address(this), "COToken: Cannot transfer to self");
+        require(_to != address(this), "ORCAToken: Cannot transfer to self");
         return ERC20.transfer(_to, _amount);
     }
 
     function transferFrom(address _from, address _to, uint256 _amount) public override notPaused returns(bool) {
-        require(_to != address(this), "COToken: Cannot transfer to self");
+        require(_to != address(this), "ORCAToken: Cannot transfer to self");
         return ERC20.transferFrom(_from, _to, _amount);
     }
 }
